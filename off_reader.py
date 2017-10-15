8686f8ea6b9a283126d65b66b96b5748b919c3f8
Jordan Levin <jlevin38@binghamton.edu>
2017-10-15 00:58:47 -0400

    it sortof works but need to fix reading in the faces

--- off_reader.py
+++ off_reader.py
@@ -1,8 +1,12 @@
 def read_file(path):
+    '''
+    Takes a path to an off file. reads the data in and returns a tuple
+    containing a list of points and a list of faces
+    '''
     ret_vertices = []
     ret_faces = []
     with open(path) as file:
-        identifier = file.readline()
+        identifier = file.readline().strip()
         if identifier != "OFF":
             return -1
         numbers = file.readline()
@@ -12,23 +16,34 @@ def read_file(path):
         for i in range(vertices):
             line = file.readline()
             line = line.split()
-            line = [float[i] for i in line]
+            line = [float(i) for i in line]
             ret_vertices.append((line[0], line[1], line[2]))
         for i in range(faces):
+            #DOESNT WORK
+            #seems the first num on the line represents how many come after
+            #need to account for that
             line = file.readline()
             line = line.split()
-            line = [int[i] for i in line]
+            line = [int(i) for i in line]
             ret_faces.append((line[0], line[1], line[2]))
     return (ret_vertices, ret_faces)
 
 
 def off_print(data):
-'''
-Takes a tuple of lists of tuples representing an off file and nicely prints it
-'''
+    '''
+    Takes a tuple of lists of tuples representing an off file and nicely prints it
+    '''
+    for l in data:
+        for points in l:
+            print('x: {0}, y: {1}, z: {2}'.format(points[0], points[1], points[2]))
 
 
 def main():
+    test = read_file("chair_0890.off")
+    if test == -1:
+        print("Error")
+        return -1
+    off_print(test)
 
 
 
